931c8af0f087365388f3cddd8600aa739da55c76
Akshu26 <akshua792@gmail.com>
2021-11-03 19:54:29 +0530

    adding description as given

---
+++ Course1.py
@@ -0,0 +1,11 @@
+What are the key concepts you learnt from these courses
+You can choose one module from the list below:-
+1)Algorithm Analysis
+2)Divide and Conquer Algorithms
+3)Dynamic Programming
+
+
+Akshatha Kantharaju
+Algorithm Analysis
+Understanding the version control system, creating the github account, modifying th existing files in github and how
+to use git commands and navigating the github and also about git, Visual Studio Code.
